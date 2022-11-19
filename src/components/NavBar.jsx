b97fb3181c2f684bc6b0218007d03d11d76649af
Zuljaro <rojasz@uninorte.edu.co>
2022-11-18 21:09:53 -0500

    Creacion de componente NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export function NavBar() {
+  return (
+    
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+    <div className="container px-5">
+        <a className="navbar-brand fw-bold" href="#page-top">InstaYa</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i className="bi-list"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ms-auto me-4 my-3 my-lg-0">
+                <li className="nav-item"><a className="nav-link me-lg-3" href="#features">Ingresar</a></li>
+                <li className="nav-item"><a className="nav-link me-lg-3" href="#download">Registrar</a></li>
+                <li className="nav-item"><a className="nav-link me-lg-3" href="#download">Portafolio</a></li>
+                <li className="nav-item"><a className="nav-link me-lg-3" href="#download">Mi Cuenta</a></li>
+            </ul>
+            
+        </div>
+    </div>
+</nav>
+
+
+
+
+    )
+}
---
+++ src/components/NavBar2.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export function NavBar2() {
+  return (
+    <footer className="bg-black text-center py-5">
+        <div className="container px-5">
+            <div className="text-white-50 small">
+                <div className="mb-2">&copy; Your Website 2022. All Rights Reserved.</div>
+                    <a href="#!">Privacy</a>
+                    <span className="mx-1">&middot;</span>
+                    <a href="#!">Terms</a>
+                    <span className="mx-1">&middot;</span>
+                    <a href="#!">FAQ</a>
+                </div>
+            </div>
+    
+</footer>
+
+   
+       
+  )
+}
